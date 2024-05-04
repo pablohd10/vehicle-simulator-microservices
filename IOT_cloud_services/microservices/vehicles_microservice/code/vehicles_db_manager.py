dd97c2825111e1d4218390bc28bc03b6543d609c
pablohd10 <phidalgod10@gmail.com>
2024-05-04 23:06:32 +0200

    sesion10 final

--- IOT_cloud_services/microservices/vehicles_microservice/code/vehicles_db_manager.py
+++ IOT_cloud_services/microservices/vehicles_microservice/code/vehicles_db_manager.py
@@ -4,10 +4,10 @@ import os
 def connect_to_db():
     # Nos conectamos a la base de datos
     mydb = mysql.connector.connect(
-        host=os.getenv("DBHOST", "127.0.0.1"),
-        user=os.getenv("DBUSER", "fic_db_user"),
-        password=os.getenv("DBPASSWORD", "RP#64nY7*E*H" ),
-        database=os.getenv("DBDATABASE", "fic_data")
+        host=os.getenv("DBHOST"),
+        user=os.getenv("DBUSER"),
+        password=os.getenv("DBPASSWORD"),
+        database=os.getenv("DBDATABASE")
     )
     return mydb
 
