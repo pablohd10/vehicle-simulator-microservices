03bf66b8e2791ec07d0f66cbdcb76f7f0a99f20d
pablohd10 <phidalgod10@gmail.com>
2024-05-05 00:14:41 +0200

    sesion10 final

--- IOT_cloud_services/message_router/code/vehicle_register_interface.py
+++ IOT_cloud_services/message_router/code/vehicle_register_interface.py
@@ -5,5 +5,5 @@ def register_vehicle(data):
   host = os.getenv('VEHICLES_MICROSERVICE_ADDRESS')
   port = os.getenv('VEHICLES_MICROSERVICE_PORT')
   r, status = requests.post('http://' + host + ':' + port + '/vehicles/register', json=data)
-  print("Respuesta de la API vehicles/register: ", r, status)
+  print("Respuesta de la API vehicles/register: ", r.json(), r.status_code)
   return r.json()
