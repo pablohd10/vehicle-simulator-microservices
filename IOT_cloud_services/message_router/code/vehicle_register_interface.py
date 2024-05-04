d9e493ab23dc9e523c49bce98ee0f6b93791c593
pablohd10 <phidalgod10@gmail.com>
2024-05-05 00:09:16 +0200

    sesion10 final

--- IOT_cloud_services/message_router/code/vehicle_register_interface.py
+++ IOT_cloud_services/message_router/code/vehicle_register_interface.py
@@ -1,7 +1,7 @@
 import requests
 import os
 
-def register_vehicle (data):
+def register_vehicle(data):
   host = os.getenv('VEHICLES_MICROSERVICE_ADDRESS')
   port = os.getenv('VEHICLES_MICROSERVICE_PORT')
   r, status = requests.post('http://' + host + ':' + port + '/vehicles/register', json=data)
