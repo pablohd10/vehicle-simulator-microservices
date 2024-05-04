0e393a653214e79592d398157957c2ff6457caf0
pablohd10 <phidalgod10@gmail.com>
2024-05-05 00:09:22 +0200

    sesion10 final

--- IOT_cloud_services/message_router/code/telemetry_register_interface.py
+++ IOT_cloud_services/message_router/code/telemetry_register_interface.py
@@ -1,7 +1,7 @@
 import requests
 import os
 
-def register_telemetry (data):
+def register_telemetry(data):
   host = os.getenv('TELEMETRY_MICROSERVICE_ADDRESS')
   port = os.getenv('TELEMETRY_MICROSERVICE_PORT')
   r = requests.post('http://' + host + ':' + port + '/telemetry/register', json=data)
